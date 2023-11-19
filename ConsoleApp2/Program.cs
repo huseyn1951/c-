27c31e5b9138f087d7e214cac558cbd3b3f4ba0d
huseyn1951 <sultanlkhuseyn@gmail.com>
2023-11-19 12:14:18 +0100

    c# code

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+
+            int num1 = 31;
+            int num2 = 69;
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+
+            int huseynAge = 19; //camelCase
+            int TuralAge = 27;  //PascalCase
+            int imaret_age = 14; //snake_case
+
+            Console.WriteLine(huseynAge);
+            Console.WriteLine(TuralAge);
+            Console.WriteLine(imaret_age);
+            Console.WriteLine("IMARET 14 YEARS!!");
+
+
+
+            string name = "Qurban";
+            string surname = "Qurbanov";
+            Console.WriteLine(name);
+            Console.WriteLine(surname);
+            Console.WriteLine(name+surname);
+
+            bool haveBallonDor = false;
+            Console.WriteLine(haveBallonDor);
+
+            bool azerbaycanCempionu = true;
+            Console.WriteLine(azerbaycanCempionu);
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("1951");
+        }
+    }
+}
